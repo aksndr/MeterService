055f65e960fd94f48a98f7b09e8f039abc17e721
aksndr <aksndr@gmail.com>
2014-09-30 23:02:13 +0400

    Initial commit

---
+++ src/main/java/ru/aksndr/MvcConfig.java
@@ -0,0 +1,17 @@
+package ru.aksndr;
+
+/**
+ * Created by aksndr on 30.09.2014.
+ */
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("hello");
+    }
+}
